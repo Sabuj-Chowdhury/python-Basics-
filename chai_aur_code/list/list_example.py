c42236531124df53420c6e67d604da7ef57c4de2
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-04-12 16:04:32 +0530

    list

---
+++ chai_aur_code/list/list_example.py
@@ -0,0 +1,28 @@
+list_example = ['sabuj', 'red', 'green', 'blue', 'yellow']
+for item in  list_example:
+    # print(item)
+    # print(item, end=" ")
+
+
+
+
+
+
+
+
+
+# print(list_example[1:1])
+# print()
+
+
+# print(list_example[0])
+
+# print(list_example[-1])  # last element
+
+# print(list_example[:])  # slice
+# print(list_example[1:])
+# list_example[0]='purple'
+# print(list_example)
+
+# list_example[1:2]= ["orange", "pink"]
+# print(list_example)
